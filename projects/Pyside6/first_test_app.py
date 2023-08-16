6a19020706a73fbfe99c2f4fc44de0ef179a6aca
JeffreyDinackus <jeffreydinackus@proton.me>
2023-08-15 23:06:03 -0400

    first Pyside6 project being worked on

--- projects/Pyside6/first_test_app.py
+++ projects/Pyside6/first_test_app.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtWidgets import QLabel, QMainWindow,QCheckBox, QComboBox, QDateEdit, QVBoxLayout, QLineEdit
+from PySide6.QtWidgets import QLabel, QMainWindow,QCheckBox, QComboBox, QDateEdit, QVBoxLayout, QLineEdit, QPushButton
 
 
 # Only needed for access to command line arguments
@@ -10,20 +10,31 @@ import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        layout = QVBoxLayout()
-        self.setWindowTitle("My App")
+        super().__init__()
+        self.init_ui()
 
-        widgets = [
-            
-            QLineEdit,
-            ]
-        for widget in widgets:
-          layout.addWidget(widget())
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
+    def init_ui(self):
+        self.setWindowTitle("NFT Minter")
+        self.setGeometry(100, 100, 1200, 400)
 
-        self.setCentralWidget(central_widget)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        self.label = QLabel("Enter Data")
+        self.layout.addWidget(self.label)
+
+        self.label = QLineEdit()
+        self.layout.addWidget(self.label)
+        self.label = QLabel("Add API Key")
+        self.layout.addWidget(self.label)
+        self.label = QLineEdit()
+        self.layout.addWidget(self.label)
+        self.button = QPushButton("Create NFT")
+        self.layout.addWidget(self.button)
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
