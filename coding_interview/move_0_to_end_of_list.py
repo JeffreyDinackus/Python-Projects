2f3d7a74800da4130c9a66e1b08ce92c4abc4b91
JeffreyDinackus <jeffreydinackus@proton.me>
2023-08-16 12:53:17 -0400

    move 0 to end of list now works with strings

--- coding_interview/move_0_to_end_of_list.py
+++ coding_interview/move_0_to_end_of_list.py
@@ -1,13 +1,19 @@
 
-#any list you want
-list = [0, 1, 0, 3, 12]
+#any list1 you want
+list1 = ['0', 0, '0', 1, 0, '0', 3, 12]
 
 
-for item in list:
+for item in list1:
     if item == 0:
         # removes the current item if it equals 0
-        list.remove(item)
-        # append function always adds at end of list
-        list.append(item)
+        list1.remove(item)
+        # append function always adds at end of list1
+        list1.append(item)
+    if item == '0':
+        # removes the current item if it equals 0
+        list1.remove(item)
+        # append function always adds at end of list1
+        list1.append(item)
 
-print(list)
+
+print(list1)
