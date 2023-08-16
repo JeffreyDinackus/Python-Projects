1d02ff6141d8a84bedb072aa7b0f881210e71908
JeffreyDinackus <jeffreydinackus@proton.me>
2023-08-16 12:35:24 -0400

    add working much better, fixed a bug with multiple widget overwriting the same name, added some button functionality!

--- projects/Pyside6/first_test_app.py
+++ projects/Pyside6/first_test_app.py
@@ -1,21 +1,20 @@
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtWidgets import QLabel, QMainWindow,QCheckBox, QComboBox, QDateEdit, QVBoxLayout, QLineEdit, QPushButton
 
-
+import requests
 # Only needed for access to command line arguments
 import sys
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 
-class MainWindow(QMainWindow):
+
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.init_ui()
+        # self.setGeometry(100, 60, 1000, 800)
 
-    def init_ui(self):
         self.setWindowTitle("NFT Minter")
-        self.setGeometry(100, 100, 1200, 400)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
@@ -23,20 +22,44 @@ class MainWindow(QMainWindow):
         self.layout = QVBoxLayout()
         self.central_widget.setLayout(self.layout)
 
-        self.label = QLabel("Enter Data")
-        self.layout.addWidget(self.label)
+        self.quantity = QLabel("Enter a quantity(optional)")
+        self.layout.addWidget(self.quantity)
 
-        self.label = QLineEdit()
-        self.layout.addWidget(self.label)
-        self.label = QLabel("Add API Key")
-        self.layout.addWidget(self.label)
-        self.label = QLineEdit()
-        self.layout.addWidget(self.label)
-        self.button = QPushButton("Create NFT")
+        self.quantity_input = QLineEdit()
+        self.layout.addWidget(self.quantity_input)
+
+
+        self.api_label = QLabel("Add API Key")
+        self.layout.addWidget(self.api_label)
+
+        self.api_input = QLineEdit()
+        self.layout.addWidget(self.api_input)
+
+        self.name_label = QLabel("Enter Name for NFT")
+        self.layout.addWidget(self.name_label)
+
+        self.name_input = QLineEdit()
+        self.layout.addWidget(self.name_input)
+
+        self.button = QPushButton("Mint NFT")
+        self.button.clicked.connect(self.button_click)
         self.layout.addWidget(self.button)
 
+    def button_click(self):
+        api_key = self.api_input.text()
+        nft_name = self.name_input.text()
+        quantity_input = self.quantity_input.text()
+        print("API Key:", api_key)
+        print("NFT Name:", nft_name)
+        print("Quantity:", quantity_input)
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
